0932a5763359c138bd8ef7e34b903be21686ba36
Nikolaj Lund Sørensen <nikolajlundsorensen@gmail.com>
2024-11-25 20:51:31 +0100

    Remove Klubadmin

--- main.go
+++ main.go
@@ -2,30 +2,6 @@
 // Click here and start typing.
 package main
 
-import (
-	"GoExperiments/dfu_klubadmin/External/Klubadmin"
-	"fmt"
-)
-
 func main() {
-	fmt.Println("Go Experiments yay")
-	fmt.Println("===========")
-	search, err := Klubadmin.Search("Lenschow")
-	for i, value := range search {
-		fmt.Printf("SearchResult Person[%v]:%+v\n", i, value)
-	}
-	fmt.Println("===========")
-	searchFirst := search[0]
-	person, err := Klubadmin.GetPerson(searchFirst)
-	fmt.Printf("GetPerson for:%+v\nResult:%+v\nErr:%v\n", searchFirst, person, err)
-	fmt.Println("===========")
-
-	persons, err := Klubadmin.SearchAll("Lund")
-	for i, value := range persons {
-		fmt.Printf("SearchAll Person[%v]:%+v\n", i, value)
-	}
-	fmt.Println("===========")
-	fmt.Println("DONE")
-
 	//errors_experiments.RunErrorsAsTests()
 }
